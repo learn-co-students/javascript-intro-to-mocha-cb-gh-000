d63ee990a6286454ec49edbc3a858e5674170bab
Julia Nara <julianara888@gmail.com>
2018-01-14 16:00:45 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+	return true;
+}
+
+function sayHey() {
+	return "Hey!";
+}
+
+function sayHeyFriend(name) {
+	return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(iceCream) {
+	return `I love ${iceCream}`;
+}
+
+function shouting(shout) {
+	return shout.toUpperCase();
+
+}
+
+function roundDown(num) {
+	return Math.floor(num);
+}
