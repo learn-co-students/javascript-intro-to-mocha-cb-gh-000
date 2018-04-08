0e94a1a55cd1938c63735c091a0bec87c609bf69
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-08 17:31:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name +'!';
+}
+
+
+function favoriteIceCream(name_of_icecream) {
+  return 'I love ' + name_of_icecream;
+}
+
+function shouting(string) {
+
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+//  for(number; number = 0; number--) {
+  //  if (number % == 0){
+      return Math.floor(number);
+      //return number.toFixed(0);
+
+
+}
